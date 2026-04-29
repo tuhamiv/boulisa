35f73d629f15de42a9a208d578b231ef65528499
Tuhami <tuhami.vc@gmail.com>
2026-04-29 16:20:41 +0300

    chore(frontend): update path aliases and routing for feature-based structure

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from "vitest/config"
-import path from "path"
+import { defineProject, mergeConfig } from "vitest/config"
+import viteConfig from "./vite.config"
 
-export default defineConfig({
-  test: {
-    globals: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./app"),
-    },
-  },
-})
+export default mergeConfig(
+  viteConfig,
+  defineProject({
+    test: {
+      globals: true,
+    }
+  })
+)
