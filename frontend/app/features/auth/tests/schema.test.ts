e84dee997babbdd19415cc446590653fed5ec49a
Tuhami <tuhami.vc@gmail.com>
2026-04-30 04:32:37 +0300

    test(auth): add data-driven unit tests for billing name on card validation logic

--- frontend/app/features/auth/tests/schema.test.ts
+++ frontend/app/features/auth/tests/schema.test.ts
@@ -32,3 +32,68 @@ describe("Validate National ID Logic", () => {
   })
 
 })
+
+describe("Validate Name on Card Logic", () => {
+  const testCases = [
+    {
+      description: "Valid, violate minimum length",
+      nameOnCard: "N",
+      expected: false,
+    },
+    {
+      description: "Valid, violate maximum length",
+      nameOnCard:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      expected: false,
+    },
+    {
+      description: "Invalid, violate allowed characters",
+      nameOnCard: "N#me",
+      expected: false,
+    },
+    {
+      description: "Invalid, violate allowed characters",
+      nameOnCard: "4587",
+      expected: false,
+    },
+    {
+      description: "Valid, follow allowed characters",
+      nameOnCard: "I'm",
+      expected: true,
+    },
+    {
+      description: "Valid, follow allowed characters",
+      nameOnCard: "Lily-Rose",
+      expected: true,
+    },
+    {
+      description: "Invalid, violate starting rule",
+      nameOnCard: "-ame",
+      expected: false,
+    },
+    {
+      description: "Invalid, violate starting rule",
+      nameOnCard: "#@",
+      expected: false,
+    },
+    {
+      description: "Invalid, violate ending rule",
+      nameOnCard: "ame-",
+      expected: false,
+    },
+    {
+      description: "Valid, Arabic name support",
+      nameOnCard: "محمد بن عبدالمطلب",
+      expected: true,
+    },
+    {
+      description: "Valid, handle normalization (trimming)",
+      nameOnCard: "   John Doe   ",
+      expected: true,
+    }
+  ]
+
+  it.each(testCases)("$description ($nameOnCard)", ({ nameOnCard, expected }) => {
+    expect(formSchema.shape.billing.shape.nameOnCard.safeParse(nameOnCard).success).toBe(expected);
+  })
+})
