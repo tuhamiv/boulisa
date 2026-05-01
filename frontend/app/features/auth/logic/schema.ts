1d6d75e2a50716b1cba3229857f2748d8857cfb8
Tuhami <tuhami.vc@gmail.com>
2026-05-02 00:28:10 +0300

    feat(auth): implement Luhn algorithm and brand-specific validation for card numbers

--- frontend/app/features/auth/logic/schema.ts
+++ frontend/app/features/auth/logic/schema.ts
@@ -60,63 +60,102 @@ const formSchema = z.object({
       path: ["password"],
     }),
 
-  profile: z
-    .object({
-      firstName: nameConstraints,
-      fatherName: nameConstraints,
-      grandfatherName: nameConstraints,
-      familyName: nameConstraints,
-      nationalId: z
-        .string()
-        .regex(nationalIdRegex, "Invalid national id")
-        .refine((nid) => {
-        const century = Number(nid.substring(0, 1));
-        const year = (century === 2 ? 1900 : 2000) + Number(nid.substring(1, 3));
-        const month = Number(nid.substring(3, 5)) - 1; // As date month format is 0:11 not 1:12
-        const day = Number(nid.substring(5, 7));
-        const date = new Date(year, month, day);
+  profile: z.object({
+    firstName: nameConstraints,
+    fatherName: nameConstraints,
+    grandfatherName: nameConstraints,
+    familyName: nameConstraints,
+    nationalId: z
+      .string()
+      .regex(nationalIdRegex, "Invalid national id")
+      .refine(
+        (nid) => {
+          const century = Number(nid.substring(0, 1))
+          const year =
+            (century === 2 ? 1900 : 2000) + Number(nid.substring(1, 3))
+          const month = Number(nid.substring(3, 5)) - 1 // As date month format is 0:11 not 1:12
+          const day = Number(nid.substring(5, 7))
+          const date = new Date(year, month, day)
 
-        return (
-          year === date.getFullYear() &&
-          month === date.getMonth() &&
-          day === date.getDate() &&
-          ((century === 2 && year >= 1960 && year <= 1999) ||
-            (century === 3 && year >= 2000 && year <= 2005))
-        )
-      }, { message: "Invalid national id (check your birthdate)" })
-        .refine((nid) => {
-        const modulus = 11;
-        const checksum= Number(nid.slice(-1));
-        const weights= [2, 7, 6, 5, 4, 3, 2, 7, 6, 5, 4, 3, 2];
-        const digits= Array.from(nid, Number);
-        const sum= weights.reduce((accumulator, currentValue, currentIndex) => accumulator + (currentValue * (digits.at(currentIndex) ?? 0)), 0);
-        const remainder= sum % modulus;
-        const calcChecksum= (() => {
-          switch (remainder) {
-            case 0: return 1
-            case 1: return 0
-            default: return modulus - remainder;
-          }
-        })();
-        return calcChecksum === checksum;
-      }, {message: "Invalid national id"}),
-      mobile: z.string().regex(/^01[0125]\d{8}$/, "Invalid mobile number"),
+          return (
+            year === date.getFullYear() &&
+            month === date.getMonth() &&
+            day === date.getDate() &&
+            ((century === 2 && year >= 1960 && year <= 1999) ||
+              (century === 3 && year >= 2000 && year <= 2005))
+          )
+        },
+        { message: "Invalid national id (check your birthdate)" }
+      )
+      .refine(
+        (nid) => {
+          const modulus = 11
+          const checksum = Number(nid.slice(-1))
+          const weights = [2, 7, 6, 5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
+          const digits = Array.from(nid, Number)
+          const sum = weights.reduce(
+            (accumulator, currentValue, currentIndex) =>
+              accumulator + currentValue * (digits.at(currentIndex) ?? 0),
+            0
+          )
+          const remainder = sum % modulus
+          const calcChecksum = (() => {
+            switch (remainder) {
+              case 0:
+                return 1
+              case 1:
+                return 0
+              default:
+                return modulus - remainder
+            }
+          })()
+          return calcChecksum === checksum
+        },
+        { message: "Invalid national id" }
+      ),
+    mobile: z.string().regex(/^01[0125]\d{8}$/, "Invalid mobile number"),
   }),
 
   billing: z.object({
     plan: z.enum(["pro", "elite"]),
     nameOnCard: z
-        .string()
-        .transform(noc => noc.trim().normalize("NFKC").toLowerCase())
-        .pipe(z
+      .string()
+      .transform((noc) => noc.trim().normalize("NFKC").toLowerCase())
+      .pipe(
+        z
           .string()
           .min(2, "Must be at least 2 letters")
           .max(50, "Must be at most 50 letters")
           .regex(/^[\p{L}\s'-]+$/u, "Letters, spaces, ' and - only")
           .regex(/^\p{L}/u, "Must start with a letter")
           .regex(/\p{L}$/u, "Must end with a letter")
-        ),
-    cardNumber: z.string(),
+      ),
+    cardNumber: z
+      .string()
+      .pipe(z
+        .string()
+        .refine((cdn) => /^[245]/.test(cdn), {
+          message: "Unsupported card number",
+        })
+        .length(16, "Card number must be 16 digits")
+        .refine(
+          (cdn) => {
+            const cardDigits = Array.from(cdn, Number)
+            const len = cardDigits.length
+            let sum = 0
+            for (let i = 0; i < len; i++) {
+              let digit = cardDigits[len - i - 1]
+              if (i % 2 === 1) {
+                digit *= 2
+                if (digit > 9) digit -= 9
+              }
+              sum += digit
+            }
+            return sum % 10 === 0
+          },
+          { message: "Invalid card number" }
+        )
+      ),
     expiryDate: z.string(),
     cvc: z.string(),
   }),
