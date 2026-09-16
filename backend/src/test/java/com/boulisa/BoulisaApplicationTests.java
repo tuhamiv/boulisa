d9f33b08c6f5e4c2e45c7dc8595e9ab31421cd1c
Tuhami <tuhami.vc@gmail.com>
2026-09-16 15:39:50 +0300

    chore(backend): initialize spring boot project with maven

---
+++ backend/src/test/java/com/boulisa/BoulisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boulisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoulisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
